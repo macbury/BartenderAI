537a80b07c25f08691f08fecfe9e1d853b36d1b6
Arkadiusz Buras <me@macbury.ninja>
2019-08-05 22:18:16 +0200

    Css render onboard

--- drink_db/app/javascript/packs/agumented_reality/index.js
+++ drink_db/app/javascript/packs/agumented_reality/index.js
@@ -21,14 +21,16 @@ export default class AgumentedReality {
     container.appendChild(this.glRenderer.domElement)
 
     this.container = container
-    // this.cssRenderer = new CSS3DRenderer()
-    // this.cssRenderer.setSize(window.innerWidth, window.innerHeight)
-    // container.appendChild(this.cssRenderer.domElement)
+    this.cssRenderer = new CSS3DRenderer()
+    this.cssRenderer.setSize(window.innerWidth, window.innerHeight)
+    container.appendChild(this.cssRenderer.domElement)
 
     this.scene = new THREE.Scene()
 
-    this.camera = new THREE.PerspectiveCamera()
-    this.scene.add(this.camera)
+    this.uiCamera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 10000 );
+    this.uiCamera.position.z = 3000;
+    this.glCamera = new THREE.PerspectiveCamera()
+    this.scene.add(this.glCamera)
 
     this.arToolkitSource = new THREEx.ArToolkitSource({
       sourceType : 'webcam'
@@ -41,24 +43,25 @@ export default class AgumentedReality {
 
     this.arToolkitSource.init(this.onWindowResize)
     this.arToolkitContext.init(() => {
-      this.camera.projectionMatrix.copy(this.arToolkitContext.getProjectionMatrix())
+      this.glCamera.projectionMatrix.copy(this.arToolkitContext.getProjectionMatrix())
       this.onWindowResize()
     })
 
-    // this.markerRoot = new THREE.Group()
-    // this.scene.add(this.markerRoot)
+    this.markerRoot = new THREE.Group()
+    this.scene.add(this.markerRoot)
 
-    this.markerControls = new THREEx.ArMarkerControls(this.arToolkitContext, this.camera, {
-      type : 'pattern',
-      patternUrl : markerPatternUrl,
-      changeMatrixMode: 'cameraTransformMatrix'
-    })
-
-    // this.markerControls = new THREEx.ArMarkerControls(this.arToolkitContext, this.markerRoot, {
-    //   type: 'pattern', 
-    //   patternUrl: markerPatternUrl,
-    //   smooth: true,
+    // this.markerControls = new THREEx.ArMarkerControls(this.arToolkitContext, this.camera, {
+    //   type : 'pattern',
+    //   patternUrl : markerPatternUrl,
+    //   changeMatrixMode: 'cameraTransformMatrix',
+    //   smooth: true
     // })
+
+    this.markerControls = new THREEx.ArMarkerControls(this.arToolkitContext, this.markerRoot, {
+      type: 'pattern', 
+      patternUrl: markerPatternUrl,
+      smooth: false,
+    })
 
     window.addEventListener('resize', this.onWindowResize)
     document.body.style.overflow = 'hidden'
@@ -78,35 +81,37 @@ export default class AgumentedReality {
     const object = new CSS3DObject(element)
     this.scene.add(object)
 
-    var geometry	= new THREE.CubeGeometry(1,1,1);
-    var material	= new THREE.MeshNormalMaterial({
-      transparent : true,
-      opacity: 0.5,
-      side: THREE.DoubleSide
-    });
+    var geometry	= new THREE.PlaneGeometry(1,1);
+    var material	= new THREE.MeshNormalMaterial();
 
     var mesh	= new THREE.Mesh( geometry, material );
-	  mesh.position.y	= geometry.parameters.height/2
-    this.scene.add(mesh)
+    mesh.position.y	= geometry.parameters.height/2
+    
+    this.markerRoot.add(mesh)
   }
 
   onWindowResize = () => {
     this.arToolkitSource.onResizeElement()
     this.arToolkitSource.copyElementSizeTo(this.glRenderer.domElement)
     if( this.arToolkitContext.arController !== null ){
-      this.camera.projectionMatrix.copy(this.arToolkitContext.getProjectionMatrix())
+      this.glCamera.projectionMatrix.copy(this.arToolkitContext.getProjectionMatrix())
 			this.arToolkitSource.copyElementSizeTo(this.arToolkitContext.arController.canvas)
-		}
+    }
+    
+    this.uiCamera.aspect = window.innerWidth / window.innerHeight
+    this.uiCamera.updateProjectionMatrix()
+
+    this.cssRenderer.setSize( window.innerWidth, window.innerHeight )
   }
 
   render = () => {
     requestAnimationFrame(this.render)
     if(this.arToolkitSource.ready) {
       this.arToolkitContext.update(this.arToolkitSource.domElement)
-      this.scene.visible = this.camera.visible
+      this.scene.visible = this.glCamera.visible
     }
     
-    this.glRenderer.render(this.scene, this.camera)
-    // this.cssRenderer.render(this.scene, this.camera)    
+    this.glRenderer.render(this.scene, this.glCamera)
+    this.cssRenderer.render(this.scene, this.uiCamera)    
   }
 }
